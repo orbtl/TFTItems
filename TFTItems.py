78ed2c6bacf14909561590792aaee12511427326
orbtl <twinsendude@gmail.com>
2019-08-17 14:52:13 -0700

    Creating basic dictionary of base items

---
+++ TFTItems.py
@@ -0,0 +1,10 @@
+baseItems = {
+    "BF": "B.F. Sword",
+    "CV": "Chain Vest",
+    "GB": "Giant's Belt",
+    "NLR": "Needlessly Large Rod",
+    "RB": "Recurve Bow",
+    "Tear": "Tear of the Goddess",
+    "Spat": "Spatula"
+}
+print(baseItems["BF"])
