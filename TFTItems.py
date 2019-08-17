718699b1446f93462ed047538e7f056dd2877972
orbtl <twinsendude@gmail.com>
2019-08-17 15:54:23 -0700

    Moved to Functions

--- TFTItems.py
+++ TFTItems.py
@@ -7,36 +7,39 @@ baseItems = {
     "Tear": "Tear of the Goddess",
     "Spat": "Spatula"
 }
+userInventory = []
 
 def printInventory():
     inventoryString = ', '.join([baseItems[item] for item in userInventory])
     print("Your current items: " + inventoryString)
 
+def printBaseItems():
+    print(f"""These are the available item codes, followed by their name
+    BF: {baseItems["BF"]}
+    CV: {baseItems["CV"]}
+    GB: {baseItems["GB"]}
+    NLR: {baseItems["NLR"]}
+    RB: {baseItems["RB"]}
+    Tear: {baseItems["Tear"]}
+    Spat: {baseItems["Spat"]}
+    """)
 
-print(f"""These are the available item codes, followed by their name
-BF: {baseItems["BF"]}
-CV: {baseItems["CV"]}
-GB: {baseItems["GB"]}
-NLR: {baseItems["NLR"]}
-RB: {baseItems["RB"]}
-Tear: {baseItems["Tear"]}
-Spat: {baseItems["Spat"]}
-""")
-userInventory = []
-print("Enter your items, separated by Enter.  When you are done press enter again.")
-isEmpty = False
-while isEmpty == False:
-    newItem = input("Item? ")
-    if len(newItem) > 0:
-        if newItem in baseItems:
-            userInventory.append(newItem)
-            print(f"You added a {baseItems[newItem]}")
+def getItems():
+    print("Enter your items, separated by Enter.  When you are done press enter again.")
+    isEmpty = False
+    while isEmpty == False:
+        newItem = input("Item? ")
+        if len(newItem) > 0:
+            if newItem in baseItems:
+                userInventory.append(newItem)
+                print(f"You added a {baseItems[newItem]}")
+            else:
+                print("Invalid Item Code.  Try again.")
         else:
-            print("Invalid Item Code.  Try again.")
-    else:
-        isEmpty = True
-        printInventory()
+            isEmpty = True
+            printInventory()
 
 
-
+printBaseItems()
+getItems()
 
